51b131aae9ee8718fed9369dc9c9a6703821ec07
Hackbug <anthony.vuillemin@outlook.fr>
2020-11-24 00:53:14 +0100

    Enhance comment design and make readonly field visible in admin page

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Task, Comment
-# Register your models here.
-admin.site.register(Task)
-admin.site.register(Comment)
+
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ('project', 'user', 'creation_date')
+
+class CommentAdmin(admin.ModelAdmin):
+    readonly_fields = ('task', 'user', 'creation_date')
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Comment, CommentAdmin)
