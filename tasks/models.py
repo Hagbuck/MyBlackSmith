ef7a1e5ed350b8d66924d07b595531c426b1fa16
Hackbug <anthony.vuillemin@outlook.fr>
2020-11-28 14:05:13 +0100

    Task have ManyToManyField relation with Label, but The user cannot link them yet

--- tasks/models.py
+++ tasks/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth import get_user_model
 
 from projects.models import Project
+from labels.models import Label
 
 class Task(models.Model):
     project = models.ForeignKey(Project, on_delete = models.CASCADE, blank = True, null = True)
@@ -10,9 +11,10 @@ class Task(models.Model):
     name = models.CharField(max_length = 256)
     text = models.TextField()
     creation_date = models.DateTimeField(default=timezone.now, editable=False)
+    labels = models.ManyToManyField(Label)
 
     def __str__(self):
-        return self.name
+        return self.name if not self.project else "{}@{}".format(self.name, self.project)
 
 class Comment(models.Model):
     task = models.ForeignKey(Task, on_delete = models.CASCADE, editable=False)
